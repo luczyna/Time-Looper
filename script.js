b6208fcec35c5d2a21c48f51b7f6e5bdbd6a8675
Ana Lu <luczynaki@gmail.com>
2014-09-09 21:12:09 -0500

    fresh start for time looper

--- script.js
+++ script.js
@@ -1,150 +1,24 @@
-(function($) {
-$(document).ready(function() {
-	
-	// set up er'thang
+/* Time Loopr will help
+ * those who have repeating tasks
+ * and like good pictures
+ */
 
-	// add more lists to the timer set
-	$('#addMore').click(function() {
-		var timeSet = $('.time-set').eq(0).clone(true, true);
-		$(this).parent().find('ul').append(timeSet);
-	});
+var loopr = {
+	timers: [],
+	timerCount: 0,
+	colours: [],
+	start: function() {
+		//initialize the timer
 
-	$('.time-delete').click(function() {
-		//only delete it if it isn't the last one available
-		if ($('.time-set').length !== 1) {
-			$(this).fadeOut(200, function() {
-				$(this).parent().slideUp(300, function() {
-					$(this).remove();
-				});
-			});
-		} else {
-			alert('That\'s the last timer. We can\'t remove it');
-		}
-	});
-
-	var timers = [];
-	function startTimer() {
-		//get all the timers and put their information in an array
-		var timeSet = $('.time-set');
-		for (var i = 0; i < timeSet.length; i++) {
-			var prompt = timeSet.eq(i).find('.time-prompt').text();
-			var amount = timeSet.eq(i).find('.time-entry').find('span').text();
-			
-			//we are given a string instead of a number for amount
-			amount = parseInt(amount, 10);
-			if (!$.isNumeric(amount)) {
-				amount = window.prompt("please commit to a real time, you jackass", 'in minutes');
-			} else if (amount <= 0) {
-				amount = window.prompt("we can\'t give you time here - try again", 'smartass');
-			}
-
-			if /* still */ (!$.isNumeric(amount) || amount <= 0) {
-				smartAssLoop();
-			}
-
-			//store the information in the array
-			timers.push([prompt, amount]);
-
-			// add divs for each timer and start a timeout
-			var timer = $('<div/>', { class: 'timer' });
-
-			if (i === 0) {
-				//this is the first timer
-				timer.addClass('running');
-			} else {
-				//these are the rest of them 
-				timer.addClass('waiting');
-			}
-
-			$('#timer').append(timer);
-
-			var thePrompt = $('<p/>', {
-				text: prompt
-			});
-			timer.append(thePrompt);
-			timer.append('<div class="progress"></div>');
-			console.log('happiness all around');
-		}
-		// console.log(timers.length);
-		$('#timer').fadeIn(300);
+	},
+	pause: function() {
+		//pause the timer
+	},
+	clear: function() {
+		//end and clear the timer
 	}
+}
 
-	function animateTimer() {
-		//this is our counter for looping through the animation
-		var counter = 0;
-
-		//this is the amount of times we should loop
-		var limit = timers.length;
-		// console.log(limit);
-
-		//we're turning things into seconds for the testing
-		var timeLime = 1000;
-
-		//this is our looping variable
-		var timeInt;
-
-		// console.log(timeLime);
-		timeInt = window.setInterval(countDown, timeLime);
-		function countDown() {
-
-			if (counter === 0) {
-				counter++;
-				timeLime = timers[(counter - 1)][1] * 1000;
-			} else if ((counter - 1) == limit) {
-				clearInterval(timeInt);
-				// return false;
-			} else {
-
-				$('.progress').eq((counter - 1)).animate({
-						'right': '100%'
-					}, timeLime, 'linear', function() {
-						counter++;
-					});
-
-				console.log('waiting to see all of the counter go down');
-			}
-
-		}
-
-		// function repeat(fn, times) {
-		// 	for(var i = 0; i < times; i++) fn();
-		// 	}
-
-		// 	repeat(doSomething, xTcount);
-		// 	// ...later...
-		// 	repeat(doSomethingElse, xTcount);
-	}
-
-	function smartAssLoop() {
-		//some one is trying to trick us, Schmeagel!
-		//no one trickes us, Precious
-		do {
-			amount = prompt('try again', 'in minutes');
-		} while (!$.isNumeric(amount) || amount <= 0);
-		// return amount;
-	}
-
-	$('.control').click(function() {
-		startTimer();
-		animateTimer();
-
-		// if $(this).hasClass('non-init') {
-
-		// }
-
-	});
-
-
-});
-})(jQuery);
-
-	
-
-
-
-
-
-
-
-
-
+var items = {
+	collection: document.getElementsByCLassName('time-item');
+}
